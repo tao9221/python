3cb069feb72208602cc01e01f9059911278b8b6a
tao9221 <tao9221@163.com>
2019-10-29 20:02:55 +0800

    update v2

--- check_domain_ssl_expiredate.py
+++ check_domain_ssl_expiredate.py
@@ -10,18 +10,29 @@ import requests
 import whois
 import time
 import datetime
+from multiprocessing import Process
 
 reload(sys)
 sys.setdefaultencoding('utf-8')
-sys.path.append('weixin/')
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)),'weixin/'))
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'whois'))
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'mail'))
+logFile = os.path.join(os.path.dirname(os.path.abspath(__file__)),'logCheck.log')
 import weixin
+import mail
 
 def send(msg):
     token=weixin.get_accessToken()
-    weixin.send(token,"@all",msg)
+    weixin.send(token,"15601119330",msg)
+
+def send_mail(message):
+    try:
+        mail.send_email(title='过期预警',recervers=['625969486@qq.com','qinjuntao@gome.com.cn','aosre@gome.com.cn'],msg=message)
+    except Exception, e:
+        w_log('{0} send mail error\n'.format(message))
 
 def w_log(msg):
-    with open('/var/log/checkssl.log','a+') as f:
+    with open(logFile,'a+') as f:
         time_now = time.strftime('%Y-%m-%d %H:%M:%S')
         f.write("{} - {}".format(time_now, msg))
 
@@ -29,8 +40,12 @@ def get_ssl_mesg(domain="baidu.com"):
     s = requests.session()
     datas = {'data':domain, 'type':'sslcheck', 'arg':'443'}
     url = 'http://web.chacuo.net/netsslcheck'
-    d = s.post(url, data=datas)
-    m = json.loads(d.content)['data'][0]
+    try:
+        d = s.post(url, data=datas, timeout=120)
+        m = json.loads(d.content)['data'][0]
+    except Exception, e:
+        w_log('{0} curl ssl web error\r\n'.format(domain))
+        return {} 
     res = re.compile(u"组织/公司名称：</td><td>(.*)</td></tr><tr><td>部门/单位.*颁发机构：</td><td>(.*)</td></tr><tr><td>证书详情.*证书类型：</td><td>(.*)</td></tr><tr><td>有效时间：</td><td>(.*)</td></tr><tr><td>过期时间：：</td><td>(.*)</td></tr><tr><td>多域名")
     result = re.findall(res, m)
     if len(result) > 0:
@@ -50,7 +65,12 @@ def get_ssl_mesg(domain="baidu.com"):
     return data 
 
 def get_domain_mesg(domain="baidu.com"):
-    data = whois.whois(domain)
+    try:
+        data = whois.whois(domain)
+        time.sleep(1)
+    except:
+        w_log("{0} whois error.\r\n".format(domain))
+        return {}
     run_stat = 0
     if data['expiration_date']:
         run_stat = 1
@@ -84,7 +104,6 @@ def get_domain_mesg(domain="baidu.com"):
     return datas
 
 def get_domain_list():
-    #file = './domain.txt'
     file = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'domain.txt')
     domain_list = []
     if file:
@@ -96,35 +115,49 @@ def get_domain_list():
 
 def check_ssl(dm):
     dm_msg = get_ssl_mesg(dm)
-    if dm_msg['runstat'] == 1:
-        if int(dm_msg['gqsj']) < 60:
+    if dm_msg.get('runstat',0) == 1:
+        if int(dm_msg['gqsj']) < 30:
             ssl_data = 'SSL证书检测结果:\n域名:{ym}\n公司:{gs}\n有效时间:{yxsj}\n过期时间:还有 {gqsj} 天过期\n证书类型:{zslx}\n颁发机构:{bfjg}\n'.format(**dm_msg)
             send(ssl_data)
-            w_log(w_log('SSL证书检测:{}:{}!!!!!!!!\n'.format(dm,dm_msg['gqsj'])))
+            send_mail(ssl_data)
+            w_log('SSL证书检测:{}:{}!!!!!!!!\n'.format(dm,dm_msg['gqsj']))
         else:
             w_log('SSL证书检测:{}:{}\n'.format(dm,dm_msg['gqsj']))
 
 def check_domain(dm):
-    dm_doamin = get_domain_mesg(dm)
-    if dm_doamin['runstat'] == 1:
-        if int(dm_doamin['gqsj']) < 30:
-            domain_data = '域名检测结果:\n域名:{ym}\n申请时间:{sqsj}\n更新时间:{gxsj}\n过期时间:{yxsj}\n有效时间:还有 {gqsj} 天过期\n域名状态:{ymzt}\n'.format(**dm_doamin)
+    dm_domain = get_domain_mesg(dm)
+    if dm_domain.get('runstat',0) == 1:
+        if int(dm_domain['gqsj']) < 30:
+            domain_data = '域名检测结果:\n域名:{ym}\n申请时间:{sqsj}\n更新时间:{gxsj}\n过期时间:{yxsj}\n有效时间:还有 {gqsj} 天过期\n域名状态:{ymzt}\n'.format(**dm_domain)
             send(domain_data)
-            w_log('域名检测:{}:{}!!!!!!!!\n'.format(dm,dm_doamin['gqsj']))
+            send_mail(domain_data)
+            w_log('域名检测:{}:{}!!!!!!!!\n'.format(dm,dm_domain['gqsj']))
         else:
-            w_log('域名检测:{}:{}\n'.format(dm,dm_doamin['gqsj']))
+            w_log('域名检测:{}:{}\n'.format(dm,dm_domain['gqsj']))
 
 def run():
     dm_list = get_domain_list()
+    domain_ssl = []
+    domain_dm = []
+    start = time.time()
     if len(dm_list) > 0:
         for dm in dm_list:
             dm = dm.strip()
             if 'https' in dm:
                 dm = dm.replace('https://','')
-                check_domain(dm)
-                check_ssl(dm)
+                domain_dm.append(dm)
+                p = Process(target=check_ssl, args=(dm,))
+                domain_ssl.append(p)
+                p.start()
             else:
-                check_domain(dm)
+                domain_dm.append(dm)
+    for n in domain_ssl:
+        n.join()
+    for dm in domain_dm:
+        print(dm)
+        check_domain(dm)
+    end = time.time()
+    w_log("本次运行使用时间: {0} 秒\n".format(end-start))
 
 if __name__ == "__main__":
     run()
